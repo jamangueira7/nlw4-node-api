a001c8b163cc1ff3bfa0d92ae7fe8b13125c6c69
João Mangueira <jamjs7@gmail.com>
2021-02-23 19:14:07 -0300

    add config para as migrations

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import './database';
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.json({ message: "Hello word NLW#" });
+    return res.json({ message: "Hello word NLW#4" });
 });
 
 app.listen(3333, () => console.log("Server is running!"));
