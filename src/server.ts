0ea93df875770c2a199bdca95f8ffec4f84eced2
João Mangueira <jamjs7@gmail.com>
2021-02-22 23:05:52 -0300

    add algumas biblioteclas e mudando resposta para json

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.send("Hello word NLW#");
+    return res.json({ message: "Hello word NLW#" });
 });
 
 app.listen(3333, () => console.log("Server is running!"));
