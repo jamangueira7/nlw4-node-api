1f329b8b646a15aacc2e6948f07a3ed622344c6e
João Mangueira <jamjs7@gmail.com>
2021-02-22 23:01:22 -0300

    add primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import express from 'express';
 const app = express();
 
+app.get("/", (req, res) => {
+    return res.send("Hello word NLW#");
+});
+
 app.listen(3333, () => console.log("Server is running!"));
