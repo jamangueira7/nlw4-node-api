814596047e176a6949c5e10f6a55319bd7418d9a
João Mangueira <jamjs7@gmail.com>
2021-02-25 17:55:14 -0300

    buscando todas as surveys

--- src/controllers/SurveyController.ts
+++ src/controllers/SurveyController.ts
@@ -16,6 +16,15 @@ class SurveyController {
 
         return response.status(201).json(survey);
     }
+
+    async show(request: Request, response: Response) {
+
+        const surveysRepository = getCustomRepository(SurveysRepository);
+
+        const all = await surveysRepository.find();
+
+        return response.status(200).json(all);
+    }
 }
 
 export { SurveyController };
