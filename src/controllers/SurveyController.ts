29c1ad79aaf7109be95cc5436791140e7f120475
João Mangueira <jamjs7@gmail.com>
2021-02-25 17:46:22 -0300

    controller create feito

--- src/controllers/SurveyController.ts
+++ src/controllers/SurveyController.ts
@@ -1,30 +1,20 @@
 import { Response, Request} from 'express';
 import { getCustomRepository } from 'typeorm';
-import { UsersRepository } from '../repositories/UsersRepository';
+import { SurveysRepository } from '../repositories/SurveysRepository';
 
 class SurveyController {
     async create(request: Request, response: Response) {
-        const { name, email } = request.body;
+        const { title, description } = request.body;
 
-        const usersRepository = getCustomRepository(UsersRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository);
 
-        const userAlreadyExists = await usersRepository.findOne({
-            email
+        const survey = surveysRepository.create({
+            title, description
         });
 
-        if(userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists!"
-            })
-        }
+        await surveysRepository.save(survey);
 
-        const user = usersRepository.create({
-            name, email
-        });
-
-        await usersRepository.save(user);
-
-        return response.json(user);
+        return response.status(201).json(survey);
     }
 }
 
--- src/models/Survey.ts
+++ src/models/Survey.ts
@@ -8,7 +8,7 @@ import {
 import { v4 as uuid } from 'uuid';
 
 @Entity('survey')
-class User {
+class Survey {
     @PrimaryColumn()
     readonly id: string;
 
