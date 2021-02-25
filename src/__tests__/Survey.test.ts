fcadde4b0311eab6981b5560d5524097aa83b2fd
João Mangueira <jamjs7@gmail.com>
2021-02-25 19:40:50 -0300

    test para trazer todos os survey

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -16,6 +16,19 @@ describe("Surveys", () => {
             });
 
         expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+    });
+
+    it("should be able to get all surveys", async () => {
+        await request(app).post("/surveys")
+            .send({
+                title: "Teste de title 2",
+                description: "teste de descrição 2"
+            });
+
+        const response = await request(app).get("/surveys");
+
+        expect(response.body.length).toBe(2);
     });
 
 })
