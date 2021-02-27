3e443fb71a4c7cda590cc12a34de3bdf5b8f2f5c
João Mangueira <jamjs7@gmail.com>
2021-02-27 11:33:00 -0300

    finalizando o controller de resposta

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -11,6 +11,24 @@ class AnswerController {
         const { value } = request.params;
         const { u } = request.query;
 
+        const surveyUsersRepository = getCustomRepository(SurveysRepository);
+
+        const surveyUser = await surveyUsersRepository.findOne({
+            id: String(u),
+        });
+
+        if(!surveyUser) {
+            return response.status(400).json({
+                error: "Survay User does not exists!"
+            });
+        }
+
+        surveyUser.value = Number(value);
+
+        await surveyUsersRepository.save(surveyUser);
+
+        return response.status(200).json(surveyUser);
+
     }
 }
 
