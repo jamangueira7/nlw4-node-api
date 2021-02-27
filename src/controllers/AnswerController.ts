9c2c09e1c249a9adc3cdbf9426b7c71f0836e41f
João Mangueira <jamjs7@gmail.com>
2021-02-27 11:38:18 -0300

    limpando controller de resposta

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,10 +1,6 @@
 import { Response, Request} from 'express';
 import { getCustomRepository } from 'typeorm';
-import { UsersRepository } from '../repositories/UsersRepository';
-import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
-import SendMailService from '../services/SendMailService';
-import { resolve } from 'path';
 
 class AnswerController {
     async execute(request: Request, response: Response) {
@@ -28,7 +24,6 @@ class AnswerController {
         await surveyUsersRepository.save(surveyUser);
 
         return response.status(200).json(surveyUser);
-
     }
 }
 
