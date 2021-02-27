2b246e579390c1712bd488de7e113b3e36ac970d
João Mangueira <jamjs7@gmail.com>
2021-02-27 11:48:14 -0300

    controller de NPS pronto

--- src/controllers/NpsController.ts
+++ src/controllers/NpsController.ts
@@ -4,7 +4,37 @@ import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 
 class NpsController {
     async execute(request: Request, response: Response) {
+        const { survey_id } = request.params;
 
+        const surveyUserRepository = getCustomRepository(SurveysUsersRepository);
+
+        const surveyUsers = await surveyUserRepository.find({
+            survey_id,
+        });
+
+        const detractor = surveyUsers.filter(
+            (survey) => survey.value >= 0 && survey.value <= 6
+        ).length;
+
+        const promoters = surveyUsers.filter(
+            (survey) => survey.value >= 9 && survey.value <= 10
+        ).length;
+
+        const passive = surveyUsers.filter(
+            (survey) => survey.value >= 7 && survey.value <= 8
+        ).length;
+
+        const totalAnswers = surveyUsers.length;
+
+        const calculate = (promoters - detractor) / totalAnswers;
+
+        return response.json({
+            detractor,
+            promoters,
+            passive,
+            totalAnswers,
+            nps: calculate
+        });
     }
 }
 
