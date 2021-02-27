f41c3aa5e7ad70684956a69aed8e435fc5fceecd
João Mangueira <jamjs7@gmail.com>
2021-02-27 12:32:47 -0300

    personalizando o erro

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, {NextFunction} from 'express';
 import createConnection from './database';
 import { router } from './routes';
+import { AppErros } from './errors/AppErros';
 
 createConnection();
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof AppErros) {
+        return response.status(err.statusCode).jsson({
+           message: err.message,
+        });
+    }
+
+    return response.status(500).jsson({
+        status: "Error",
+        message: `Internal server error ${err.message}`,
+    });
+});
 
 export { app };
